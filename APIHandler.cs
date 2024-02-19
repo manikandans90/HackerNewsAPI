a7cdf91dac3d8d4224be0353eb73f49bd51b6055
Manikandan Sankar <mani.merit2008@gmail.com>
2024-02-19 18:17:58 +0530

    Format fix

--- APIHandler.cs
+++ APIHandler.cs
@@ -6,29 +6,28 @@ namespace HackerNewsAPI
 {
     public class APIHandler : IAPIHander
     {
-        private readonly HttpClient _httpClient;
+        private readonly HttpClient httpClient;
         private readonly string? serviceUrl;
         private readonly string? allStory;
-        private readonly IConfiguration _configuration;
-        private readonly ILogger<APIHandler> _logger;
-        public APIHandler(IConfiguration configuration, ILogger<APIHandler> logger)
+        private readonly IConfiguration configuration;
+        private readonly ILogger<APIHandler> logger;
+        public APIHandler(IConfiguration _configuration, ILogger<APIHandler> _logger)
         {
-            _httpClient = new HttpClient();
-            _configuration = configuration;
+            httpClient = new HttpClient();
+            configuration = _configuration;
             serviceUrl = _configuration.GetSection("ServiceUrl").Get<string>();
             allStory = _configuration.GetSection("GetStories").Get<string>();
-            _logger = logger;
+            logger = _logger;
         }
 
 
         public async Task<IEnumerable<Story>> GetStoryById(string? id)
         {
-
             string apiUrl = string.Format(serviceUrl, id);
 
             try
             {
-                HttpResponseMessage response = await _httpClient.GetAsync(apiUrl.ToString());
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl.ToString());
                 if (response.IsSuccessStatusCode)
                 {
                     var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
@@ -42,17 +41,15 @@ namespace HackerNewsAPI
                         List<Story>? storiesList = new List<Story>();
                         storiesList?.Add(story);
                         IEnumerable<Story>? storiesEnumerable = storiesList;
+
                         return storiesEnumerable;
                     }
                 }
-
                 return null;
-
-
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Unable to execute the API, Please try again");
+                logger.LogError(ex, "Unable to execute the API, Please try again");
                 throw ex;
 
             }
@@ -60,10 +57,9 @@ namespace HackerNewsAPI
 
         public async Task<List<string>> GetAllStory()
         {
-
             try
             {
-                HttpResponseMessage response = await _httpClient.GetAsync(allStory);
+                HttpResponseMessage response = await httpClient.GetAsync(allStory);
                 if (response.IsSuccessStatusCode)
                 {
                     var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
@@ -71,19 +67,15 @@ namespace HackerNewsAPI
 
                     if (!string.IsNullOrWhiteSpace(convertedValue))
                     {
-
                         var data = JsonConvert.DeserializeObject<List<String>>(convertedValue);
-
                         return data;
                     }
                 }
-
                 return null;
-
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Unable to execute the API, Please try again");
+                logger.LogError(ex, "Unable to execute the API, Please try again");
                 throw ex;
             }
         }
